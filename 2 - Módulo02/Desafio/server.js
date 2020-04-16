2a1ef82ce3e41d8d41994c8642506d6429f8253a
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-04-15 21:36:59 -0300

    Modulo04 - 01/04 Criando as Rotas de Instrutores

--- 2 - Módulo02/Desafio/server.js
+++ 2 - Módulo02/Desafio/server.js
@@ -21,6 +21,20 @@ server.get("/conteudos", function (req, res) {
     return res.render("conteudos", { conteudos: cursos })
 })
 
+server.get("/curso", function (req, res) {
+    const id = req.query.id
+    
+    const curso = cursos.find(function(curso){
+        if(curso.id == id){
+            return true
+        }
+    })
+    if(!curso){
+        return res.send("Course not Found!")
+    }
+    return res.render("curso", {conteudo: curso})
+})
+
 server.get("/sobre", function (req, res) {
     return res.render("sobre")
 })
