b0b477da232366999fa24e9d8a90e0308fe8e309
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-06-17 16:11:35 -0300

    Algoritmo de Paginação Front Funcional

--- modulo05/public/scripts.js
+++ modulo05/public/scripts.js
@@ -1,8 +1,35 @@
-const currentPage = location.pathname
-const menuItems = document.querySelectorAll("header .links a")
+const currentPage = location.pathname;
+const menuItems = document.querySelectorAll("header .links a");
 
 for (item of menuItems) {
-    if(currentPage.includes(item.getAttribute("href"))){
-        item.classList.add("active")
-    }
+  if (currentPage.includes(item.getAttribute("href"))) {
+    item.classList.add("active");
+  }
 }
+
+//Algoritmo de Paginação no Front End
+
+let totalPages = 20,
+  selectedPage = 6,
+  pages = [],
+  oldPage
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+
+  if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+
+    if (oldPage && currentPage - oldPage > 2) {
+      pages.push("...")
+    }
+    if(oldPage && currentPage - oldPage == 2){
+        pages.push(oldPage + 1)
+    }
+    pages.push(currentPage)
+    oldPage = currentPage
+  }
+}
+
+console.log(pages);
