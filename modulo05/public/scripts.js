243e6131f588ad37fd9dd77fe077730d0c5156c3
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-06-17 20:26:19 -0300

    Paginação com pontinhos

--- modulo05/public/scripts.js
+++ modulo05/public/scripts.js
@@ -10,10 +10,8 @@ for (item of menuItems) {
 //Algoritmo de Paginação no Front End
 
 function paginate(selectedPage, totalPages) {
-  let totalPages = 20,
-    selectedPage = 6,
-    pages = [],
-    oldPage;
+  
+  let pages = [], oldPage;
 
   for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
     const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
@@ -40,3 +38,15 @@ function paginate(selectedPage, totalPages) {
 const pagination = document.querySelector(".pagination")
 const page = +pagination.dataset.page
 const total = +pagination.dataset.total
+const pages = paginate(page, total)
+
+let elements = ""
+for (let page of pages) {
+  if(String(page).includes("...")){
+    elements += `<span>${page}</span>`
+  }else{
+    elements += `<a href="page=${page}">${page}</a>`
+  }
+  
+}
+pagination.innerHTML = elements
