ae6bd04a307fb461a4243ab8d99db8e50a966697
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-06-18 14:59:58 -0300

    Ajuste Final corrigindo erro de pagination

--- modulo05/public/scripts.js
+++ modulo05/public/scripts.js
@@ -10,8 +10,8 @@ for (item of menuItems) {
 //Algoritmo de Paginação no Front End
 
 function paginate(selectedPage, totalPages) {
-  
-  let pages = [], oldPage;
+  let pages = [],
+    oldPage;
 
   for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
     const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
@@ -32,27 +32,32 @@ function paginate(selectedPage, totalPages) {
       oldPage = currentPage;
     }
   }
-  return pages
+  return pages;
 }
 
-const pagination = document.querySelector(".pagination")
-const filter = pagination.dataset.filter
-const page = +pagination.dataset.page
-const total = +pagination.dataset.total
-const pages = paginate(page, total)
+function createPagination(pagination) {
+  const filter = pagination.dataset.filter;
+  const page = +pagination.dataset.page;
+  const total = +pagination.dataset.total;
+  const pages = paginate(page, total);
 
-let elements = ""
-for (let page of pages) {
-  if(String(page).includes("...")){
-    elements += `<span>${page}</span>`
-  }else{
-    if(filter){
-      elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
-    }else{
-      elements += `<a href="?page=${page}">${page}</a>`
+  let elements = "";
+  for (let page of pages) {
+    if (String(page).includes("...")) {
+      elements += `<span>${page}</span>`;
+    } else {
+      if (filter) {
+        elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`;
+      } else {
+        elements += `<a href="?page=${page}">${page}</a>`;
+      }
     }
-    
   }
-  
+  pagination.innerHTML = elements;
 }
-pagination.innerHTML = elements
+
+const pagination = document.querySelector(".pagination");
+
+if (pagination) {
+  createPagination(pagination)
+}
